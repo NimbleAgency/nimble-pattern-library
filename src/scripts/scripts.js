3735ab9c48fcaaf84c226c4c0c8b0917630028a5
Jeff Nordstedt <jeff@nimbleagency.com>
2014-08-31 16:30:19 -0400

    Initial Commit

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,38 +1,73 @@
-fluidvids.init({
-  selector: ['iframe'],
-  players: ['www.youtube.com', 'player.vimeo.com']
-});
+$( function() {
+    /**
+    * ---------------------------------------------------------------------------
+    * Main Menu
+    * ---------------------------------------------------------------------------
+    */
+    var mainMenu = $( ".sf-menu" );
 
-$("#show-navigation").on("click", function() {
-  $("body").toggleClass("show-nav");
-});
+    // Create the responsive menu by cloning the existing one.
+    mainMenu.clone().removeClass().addClass( "rwd-menu" ).prependTo( ".navbar-container nav" );
 
-$("#hide-navigation").on("click", function() {
-  $("body").toggleClass("show-nav");
-});
+    // Initialize the Superfish menu.
+    mainMenu.superfish();
 
+    // Toggle the responsive menu.
+    $( "#rwd-trigger" ).on( "click", function( e ) {
+        e.preventDefault();
 
- $(function() {        
-    $("#tabContent").organicTabs({
-        "speed": 200
+        $( ".rwd-menu" ).slideToggle();
+    } );
+
+    /**
+    * ---------------------------------------------------------------------------
+    * Animations
+    * ---------------------------------------------------------------------------
+    */
+
+    $( "#demo-animations" ).find( ".demo-col" ).on( "click", function() {
+        var $this = $( this );
+
+        $this.addClass( "animated " + $this.text() );
     });
-});
 
- jQuery(document).ready(function($) {
-  // Please note that autoHeight option has some conflicts with options like imageScaleMode, imageAlignCenter and autoScaleSlider
-  // it's recommended to disable them when using autoHeight module
-  $('#content-slider-1').royalSlider({
-    autoHeight: true,
-    arrowsNav: true,
-    fadeinLoadedSlide: false,
-    controlNavigation: 'bullets',
-    controlsInside: false,
-    imageScaleMode: 'none',
-    imageAlignCenter:false,
-    loop: false,
-    loopRewind: true,
-    numImagesToPreload: 6,
-    keyboardNavEnabled: true,
-    usePreloader: false
-  });
-});
+    /**
+    * ---------------------------------------------------------------------------
+    * Tabs
+    * ---------------------------------------------------------------------------
+    */
+
+    $( ".tabs-links a" ).on( "click", function( e ) {
+        e.preventDefault();
+
+        // Get the target tab
+        var newTab = $( this ).attr( "href" );
+
+        // Show the targeted tab content while hiding the rest
+        $( newTab ).show().siblings().hide();
+
+        // Add active class tot he parent list item while removing it from rest.
+        $( this ).parent( "li" ).addClass( "active" ).siblings().removeClass( "active" );
+    } );
+
+    /**
+    * ---------------------------------------------------------------------------
+    * Accordions
+    * ---------------------------------------------------------------------------
+    */
+    $( ".accordion-element-title" ).on( "click", function( e )  {
+        e.preventDefault();
+
+        // Get the target accordion element
+        var targetAccordion = $( this ).attr( "href" );
+
+        // Collapse all accordion elements
+        $( ".accordion-element-title" ).removeClass( "active" );
+        $( ".accordion-element-content" ).hide();
+
+        // Show the targeted element only 
+        $( this ).addClass( "active" );
+        $( targetAccordion ).slideDown();
+    } );
+
+} );
